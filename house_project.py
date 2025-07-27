caf469e03d339e16047b99872497d33f0a58bd0e
Parth Garg <parth.garg2024@vitstudent.ac.in>
2025-07-27 12:13:25 +0530

    Updated: Added model training and accuracy evaluation

--- house_project.py
+++ house_project.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(url, sep=r"\s+", skiprows=22, header=None)
 even_rows = raw_df.iloc[::2].reset_index(drop=True)
@@ -15,13 +17,11 @@ full_df.columns = column_names
 X = full_df.drop("MEDV", axis=1) 
 y = full_df["MEDV"]      
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print("X shape:", X.shape)
-print("X_train shape:", X_train.shape)
-print("X_test shape:", X_test.shape)
-print("y_train shape:", y_train.shape)
-print("y_test shape:", y_test.shape)
-
-
-
-
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("Mean Squared Error:", mse)
+print("R² Score:", r2)
 
