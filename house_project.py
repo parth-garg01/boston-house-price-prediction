f938bb6e5f20ca9b3626a017c246768f4358fb63
Parth Garg <parth.garg2024@vitstudent.ac.in>
2025-07-27 13:08:22 +0530

    removed random forest came back to linear regression and applied skew current accuracy 74 percent

--- house_project.py
+++ house_project.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+from scipy.stats import skew
 url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(url, sep=r"\s+", skiprows=22, header=None)
 even_rows = raw_df.iloc[::2].reset_index(drop=True)
@@ -15,13 +15,17 @@ column_names = [
     "MEDV" 
 ]
 full_df.columns = column_names
-X = full_df.drop("MEDV", axis=1) 
-y = full_df["MEDV"]      
+X = full_df.drop("MEDV", axis=1)
+y = full_df["MEDV"]
+skewed_feats = X.apply(lambda x: skew(x)).sort_values(ascending=False)
+skewed_cols = skewed_feats[abs(skewed_feats) > 0.75].index
+X[skewed_cols] = np.log1p(X[skewed_cols])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = RandomForestRegressor(random_state=42, n_estimators=100)
+model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print("Mean Squared Error:", mse)
 print("R² Score:", r2)
+
