a08ea0146622c1a31fb3182babd24c86ccec5bdd
Parth Garg <parth.garg2024@vitstudent.ac.in>
2025-07-27 12:28:42 +0530

    Updated:used random forest to improve the accuracy now switched from 66.7 to 89.2 percent

--- house_project.py
+++ house_project.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(url, sep=r"\s+", skiprows=22, header=None)
@@ -17,11 +18,10 @@ full_df.columns = column_names
 X = full_df.drop("MEDV", axis=1) 
 y = full_df["MEDV"]      
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = LinearRegression()
+model = RandomForestRegressor(random_state=42, n_estimators=100)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print("Mean Squared Error:", mse)
 print("R² Score:", r2)
-
